dc693ee5c5e5a000d44730d7941e38883257ae58
MarioGC <mario.gallego@live.u-tad.com>
2023-12-26 15:49:46 +0100

    Project creation

---
+++ Source/Sigurd/Sigurd.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sigurd.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sigurd, "Sigurd" );
+ 
