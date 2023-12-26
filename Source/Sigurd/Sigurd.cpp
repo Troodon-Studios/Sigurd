a168002a11c46a2195370e2f247b23e7f646f808
MarioGC <mario.gallego@live.u-tad.com>
2023-12-26 20:30:10 +0100

    Removing ignored files

--- Source/Sigurd/Sigurd.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sigurd.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sigurd, "Sigurd" );
- 
