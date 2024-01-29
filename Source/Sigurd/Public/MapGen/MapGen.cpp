dd895db40ebad94726914f140911ebc0e3d23f5d
MarioGC <mario.gallego@live.u-tad.com>
2024-01-29 18:06:08 +0100

    basic grid not working

---
+++ Source/Sigurd/Public/MapGen/MapGen.cpp
@@ -0,0 +1,57 @@
+﻿#include "MapGen.h"
+
+// Sets default values
+AMapGen::AMapGen()
+{
+    // Set this actor to call Tick() every frame
+    PrimaryActorTick.bCanEverTick = true;
+
+    // initialize the static mesh component
+    
+    // Initialize grid dimensions and seed
+    GridSize = FVector2D(10, 10);
+    ModulesSize = FIntVector(10, 10, 10);
+    Seed = 0;
+    
+}
+
+// Called when the game starts or when spawned
+void AMapGen::BeginPlay()
+{
+    Super::BeginPlay();
+    
+}
+
+// Called every frame
+void AMapGen::Tick(const float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Function to generate the grid
+void AMapGen::GenerateGrid() const
+{
+    // Get the world
+    if (UWorld* World = GetWorld())
+    {
+        const int Pos_X = GridSize.X;
+        const int Pos_Y = GridSize.Y;
+
+        // Calculate the offset based on the size of the modules
+        const FVector Offset = FVector(ModulesSize.X / 2.0f, ModulesSize.Y / 2.0f, ModulesSize.Z / 2.0f);
+
+        for (int n = 0 ; n < Pos_X ; n++)
+        {
+            for (int m = 0 ; m < Pos_Y ; m++)
+            {
+                // Calculate the position of the module
+                const FVector Position = FVector(n * ModulesSize.X, m * ModulesSize.Y, 0) - Offset;
+
+                // spawn the module on the calculated position
+                World->SpawnActor<AActor>(Module, Position, FRotator::ZeroRotator);
+                
+                
+            }
+        }
+    }
+}
---
+++ Source/Sigurd/Public/MapGen/MapGen.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "MapGen.generated.h"
+
+UCLASS()
+class AMapGen : public AActor
+{
+    GENERATED_BODY()
+
+public:	
+    // Sets default values for this actor's properties
+    AMapGen();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:	
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // actor to spawn
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Modules")
+    TSubclassOf<AActor> Module;
+    
+    // Grid dimensions
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Modules")
+    FIntVector ModulesSize;
+    
+    // Grid dimensions
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+    FVector2D GridSize;
+
+    // Seed for random generation
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid")
+    int Seed;
+
+    // Function to generate the grid
+    UFUNCTION(BlueprintCallable, Category = "Grid")
+    void GenerateGrid() const;
+};
