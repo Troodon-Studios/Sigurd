680343ba94fad0b86131f116d219236cf2f705bd
MarioGC <mario.gallego@live.u-tad.com>
2024-02-20 13:50:50 +0100

    Base enemy cpp/h

---
+++ Source/Sigurd/Private/Enemies/BaseEnemy.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemies/BaseEnemy.h"
+
+
+
+// Sets default values
+ABaseEnemy::ABaseEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	StaminaComponent = CreateDefaultSubobject<UStaminaComponent>(TEXT("StaminaComponent"));
+	CombatComponent = CreateDefaultSubobject<UCombatComponent>(TEXT("CombatComponent"));
+	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("HealthComponent"));
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(GetMesh(), FName("RH_Socket"));
+	
+}
+
+// Called when the game starts or when spawned
+void ABaseEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (StaminaComponent){
+		StaminaComponent->RegisterComponent();
+	}
+
+	if (CombatComponent){
+		CombatComponent->RegisterComponent();
+	}
+
+	if (HealthComponent){
+		HealthComponent->RegisterComponent();
+	}
+
+	OnTakeAnyDamage.AddDynamic(this, &ABaseEnemy::TakeDamageEnemy);	
+}
+
+// Called every frame
+void ABaseEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ABaseEnemy::TakeDamageEnemy(AActor* DamagedActor, float Damage, const class UDamageType* DamageType,
+										class AController* InstigatedBy, AActor* DamageCauser) {
+	UObject* ObjectInstance = const_cast<UObject*>(static_cast<const UObject*>(DamageType));
+	CombatComponent->TakeDamage(Damage, ObjectInstance);
+}
---
+++ Source/Sigurd/Public/Enemies/BaseEnemy.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CombatComponent.h"
+#include "Components/StaminaComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+#include "BaseEnemy.generated.h"
+
+UCLASS()
+class SIGURD_API ABaseEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Stamina", meta = (AllowPrivateAccess = "true"))
+	UStaminaComponent* StaminaComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	UCombatComponent* CombatComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = "true"))
+	UHealthComponent* HealthComponent;
+
+	
+public:
+	// Sets default values for this character's properties
+	ABaseEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void TakeDamageEnemy(AActor* DamagedActor, float Damage, const class UDamageType* DamageType,
+										class AController* InstigatedBy, AActor* DamageCauser);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
