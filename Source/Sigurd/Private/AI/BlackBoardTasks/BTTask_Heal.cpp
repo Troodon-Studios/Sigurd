e117d9e3c07a7c151e123d3ba6e2d85794a12bb1
MarioGC <mario.gallego@live.u-tad.com>
2024-02-21 13:45:51 +0100

    Added Tasks

---
+++ Source/Sigurd/Private/AI/BlackBoardTasks/BTTask_Heal.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BlackBoardTasks/BTTask_Heal.h"
+
+#include "AIController.h"
+#include "Components/HealthComponent.h"
+#include "Interfaces/AICoreInterface.h"
+
+UBTTask_Heal::UBTTask_Heal(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = TEXT("Heal");
+	HealAmount = 2.0f;
+}
+
+EBTNodeResult::Type UBTTask_Heal::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// Check if the pawn implements the AICoreInterface
+	if (APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn(); ControlledPawn->GetClass()->ImplementsInterface(UAICoreInterface::StaticClass()))
+	{
+
+		// get HealthComponent from the pawn
+		if (UHealthComponent* HealthComponent = IAICoreInterface::Execute_GetHealthComponent(ControlledPawn))
+		{
+			// heal the pawn
+			HealthComponent->Heal(HealAmount);
+		}
+		
+	}
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/Sigurd/Private/AI/BlackBoardTasks/BTTask_TourPatrol.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BlackBoardTasks/BTTask_TourPatrol.h"
+
+UBTTask_TourPatrol::UBTTask_TourPatrol(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = TEXT("TourPatrol");
+}
+
+EBTNodeResult::Type UBTTask_TourPatrol::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    
+    // Check if the pawn implements the AICoreInterface
+    if (APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn(); ControlledPawn->GetClass()->ImplementsInterface(UAICoreInterface::StaticClass()))
+    {
+        // Cast the pawn to the IAICoreInterface
+        const IAICoreInterface* AICore = Cast<IAICoreInterface>(ControlledPawn);
+
+        // Get the patrol route
+        APatrolRoute* PatrolRoute = AICore->Execute_GetPatrolRoute(ControlledPawn);
+
+        // use AI move to to move to the next patrol point
+        if (PatrolRoute)
+        {
+            FVector NextPatrolPoint;
+            if (PatrolRoute->GetSplinePointsAsWorldPosition(NextPatrolPoint))
+            {
+                // set blackboard value to the next patrol point
+                OwnerComp.GetBlackboardComponent()->SetValueAsVector("PointOfInterest", NextPatrolPoint);
+            }
+        }
+        
+    }
+
+    return EBTNodeResult::Succeeded;
+}
