5bab274ebb9aeb77719fb815a003d4bbd1883a81
MarioGC <mario.gallego@live.u-tad.com>
2024-02-22 17:59:54 +0100

    cleanup

--- Source/Sigurd/Public/Components/TokenComponent.h
+++ Source/Sigurd/Public/Components/TokenComponent.h
@@ -21,51 +21,53 @@ struct FToken;
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UTokenComponent : public UActorComponent
 {
- GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
- UTokenComponent();
+	UTokenComponent();
 
- UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Token")
- int32 MaxTokens = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Token")
+	int32 MaxTokens = 3;
 
- UFUNCTION(BlueprintCallable, Category="Token")
- void ReserveTokens(UTokenComponent* Caster, int32 Quantity);
+	UFUNCTION(BlueprintCallable, Category="Token")
+	void ReserveTokens(UTokenComponent* Caster, int32 Quantity);
 
- UFUNCTION(BlueprintCallable, Category="Token")
- void ReturnTokens(UTokenComponent* Owner);
+	UFUNCTION(BlueprintCallable, Category="Token")
+	void ReturnTokens(UTokenComponent* Owner);
 
- UFUNCTION(BlueprintCallable, Category="Token")
- void ReturnAllTokens();
+	UFUNCTION(BlueprintCallable, Category="Token")
+	void ReturnAllTokens();
 
- UFUNCTION(BlueprintCallable, Category="Token")
- void PrintTokenInfo();
+	UFUNCTION(BlueprintCallable, Category="Token")
+	void PrintTokenInfo();
 
 protected:
- virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
 public:
- virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-          FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
- void AddToQueue(UTokenComponent* Caster, int32 Quantity);
- void GiveToken(UTokenComponent* Caster, int32 Quantity);
- void ReceiveToken(FToken* Token);
- static void RemoveTokens(const UTokenComponent* Owner, TArray<FToken*>& TokensArray);
+	void AddToQueue(UTokenComponent* Caster, int32 Quantity);
+	void GiveToken(UTokenComponent* Caster, int32 Quantity);
+	void ReceiveToken(FToken* Token);
+	static void RemoveTokens(const UTokenComponent* Owner, TArray<FToken*>& TokensArray);
 
- TArray<FToken*> Tokens;
- TArray<FToken*> AviableTokens;
- TMap<UTokenComponent*, int32> CastersQueue;
+	TArray<FToken*> Tokens;
+	TArray<FToken*> AviableTokens;
+	TMap<UTokenComponent*, int32> CastersQueue;
 };
 
 USTRUCT(BlueprintType)
 struct FToken
 {
- explicit FToken(const int ID = 0, UTokenComponent* NewOwner = nullptr) : UniqueID(ID), Owner(NewOwner) {}
+	explicit FToken(const int ID = 0, UTokenComponent* NewOwner = nullptr) : UniqueID(ID), Owner(NewOwner)
+	{
+	}
 
- GENERATED_BODY()
+	GENERATED_BODY()
 
- uint32 UniqueID;
- UTokenComponent *Owner;
+	uint32 UniqueID;
+	UTokenComponent* Owner;
 };
